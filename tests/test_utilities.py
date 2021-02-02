ebf14cc2d09b17c20076e41df6c5d3f60ccdaaf0
herrfeder <d.lassig@t-online.de>
2021-02-02 19:42:25 +0100

    add tests

---
+++ tests/test_utilities.py
@@ -0,0 +1,12 @@
+import pytest
+import sys 
+sys.path.append('..')
+
+from app.utilities.parsezeeklogs import ParseZeekLogs
+
+pzl = ParseZeekLogs()
+
+class test_ParseZeekLogs(object):
+
+  def __init__():
+    pass

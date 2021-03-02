1d278e81c47a6d77869b9f5e9e21d64a58b8353c
herrfeder <d.lassig@t-online.de>
2021-03-02 11:12:59 +0100

    adding deployment stuff

--- .circleci/scripts/push_cloudformation_stack.sh
+++ .circleci/scripts/push_cloudformation_stack.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-aws cloudformation deploy --stack-name $1 \
---template-file $2 \
---capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
---parameter-overrides "WorkflowID=$3" \
---region=us-west-2
+STACK_EXISTS=$(aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE --query "StackSummaries[*].StackName" | grep "$1")
+
+if [ -z "$STACK_EXISTS" ]
+  then
+
+	aws cloudformation deploy --stack-name $1 \
+	--template-file $2 \
+	--capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
+	--parameter-overrides "WorkflowID=$3" \
+	--region=us-west-2
+
+  else
+	echo "Stack $1 already exists"
+  fi
+
+
